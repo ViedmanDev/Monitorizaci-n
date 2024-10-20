1200ec47ed335673919dd36feebc2bb82a4d16ed
DevCristobalvc <cristobalvalencia3002@gmail.com>
2024-10-20 00:13:39 -0500

    reproducir video en manual con sonido

--- src/app/components/Video.js
+++ src/app/components/Video.js
@@ -1,21 +1,20 @@
-  // components/YouTubeVideo.js
-  import React from 'react';
-  import "../global.css"
+// components/YouTubeVideo.js
+import React from 'react';
+import "../global.css";
 
+const YouTubeVideo = ({ videoId }) => {
+  return (
+    <div className="video-container">
+      <iframe
+        src={`https://www.youtube.com/embed/${videoId}?modestbranding=1&loop=1&playlist=${videoId}`}
+        title="YouTube video player"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerPolicy="strict-origin-when-cross-origin"
+        allowFullScreen>
+      </iframe>
+    </div>
+  );
+};
 
-  const YouTubeVideo = ({videoId}) => {
-    return (
-      <div className="video-container">
-        <iframe   
-          src={`https://www.youtube.com/embed/${videoId}?modestbranding=1&loop=1`} 
-          title="YouTube video player" 
-          frameborder="0" 
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
-          referrerpolicy="strict-origin-when-cross-origin" 
-          allowfullscreen>
-          </iframe>
-      </div>
-    );
-  };
-
-  export default YouTubeVideo; 
+export default YouTubeVideo;
