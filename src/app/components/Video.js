9f467e9a2e3a7f0907648e20807f62bbe8d96c18
DevCristobalvc <cristobalvalencia3002@gmail.com>
2024-10-19 22:58:39 -0500

    responsive para iframe de video yt

--- src/app/components/Video.js
+++ src/app/components/Video.js
@@ -1,21 +1,21 @@
-// components/YouTubeVideo.js
-import React from 'react';
+  // components/YouTubeVideo.js
+  import React from 'react';
+  import "../global.css"
 
-const YouTubeVideo = ({videoId}) => {
-  return (
-    <div >
-      <iframe 
-        width="400" 
-        height="250" 
-        src={`https://www.youtube.com/embed/${videoId}?autoplay=1&modestbranding=1`} 
-        title="YouTube video player" 
-        frameborder="0" 
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" 
-        allowfullscreen>
 
-        </iframe>
-    </div>
-  );
-};
+  const YouTubeVideo = ({videoId}) => {
+    return (
+      <div className="video-container">
+        <iframe   
+          src={`https://www.youtube.com/embed/${videoId}?autoplay=1&controls=0&modestbranding=1&showinfo=0&loop=1&playlist=${videoId}&mute=1`} 
+          title="YouTube video player" 
+          frameborder="0" 
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+          referrerpolicy="strict-origin-when-cross-origin" 
+          allowfullscreen>
+          </iframe>
+      </div>
+    );
+  };
 
-export default YouTubeVideo;
+  export default YouTubeVideo; 
