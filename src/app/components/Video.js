e97aa3222572806c4551b4722de1b7d1973870c2
DevCristobalvc <cristobalvalencia3002@gmail.com>
2024-10-20 00:18:55 -0500

    Ajuste de reproducciòn de video

--- src/app/components/Video.js
+++ src/app/components/Video.js
@@ -6,11 +6,10 @@ const YouTubeVideo = ({ videoId }) => {
   return (
     <div className="video-container">
       <iframe
-        src={`https://www.youtube.com/embed/${videoId}?modestbranding=1&loop=1&playlist=${videoId}`}
+        src={`https://www.youtube.com/embed/${videoId}?modestbranding=1&rel=0`}
         title="YouTube video player"
         frameBorder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        referrerPolicy="strict-origin-when-cross-origin"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
         allowFullScreen>
       </iframe>
     </div>
